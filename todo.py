e6fc0f836345c9630a093f2605321a4fe6fe1d7f
ItsAbir005 <abirmaity44@gmail.com>
2025-03-12 22:04:16 +0530

    change todo & index.html

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-app = Flask(__name__, template_folder='templates.html')
+app = Flask(__name__, template_folder='templates')
 todos = []
 @app.route('/')
 def index():
@@ -8,13 +8,22 @@ def index():
 def add():
     todo = request.form['todo']
     todos.append({'task': todo, 'done': False})
-    return redirect(url_for('index'))
+    return redirect(url_for('templets'))
 @app.route('/edit/<int:index>', methods=['GET', 'POST'])
 def edit(index):
     todo = todos[index]
     if request.method == 'POST':
         todo['task'] = request.form['todo']
-        return redirect(url_for('index'))
+        return redirect(url_for('templets'))
     else:
         return render_template('edit.html', todo=todo, index=index)
- 
+@app.route('/check/<int:index>')
+def check(index):
+    todos[index]['done'] = not todos[index]['done']
+    return redirect(url_for('templets'))
+@app.route('/delete/<int:index>')
+def delete(index):
+    del(todos[index])
+    return redirect(url_for('index'))
+if __name__ == '__main__':
+    app.run(debug=True)
