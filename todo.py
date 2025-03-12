8d51a0defebc01ae33cf79afb0955cbc89b95e84
ItsAbir005 <abirmaity44@gmail.com>
2025-03-12 11:40:51 +0530

    change todo.py

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates.html')
 todos = []
 @app.route('/')
 def index():
